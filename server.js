89ab691a7d6e1b34b27bfed1b22b3479943e3fe6
EC2 Default User <ec2-user@ip-172-31-47-23.ap-northeast-1.compute.internal>
2020-11-10 05:37:43 +0000

    server

--- server.js
+++ server.js
@@ -1,40 +1,7 @@
-// let express = require("express")
-// let app = express()
-
-
-// app.set('view engine','ejs');
-
-// app.get('/', function(req, res){    
-//     res.render('hello.ejs');   
-// });
-// app.get('/login',function(req,res){
-//     res.render('login.ejs')
-// })
-
-// app.get('/do_login',function(req, res) {
-//     var id = req.param('id')
-//     var pass = req.param('password')
-//     if (pass === 'password'){
-//         res.render('hello.ejs',{name:id})
-//     }else{
-//         res.render('login.ejs')
-//     }
-// })
-
-// var ip =  process.env.IP || "0.0.0.0"
-// var port = process.env.PORT || 8080
+const http = require("http")
 const port = process.env.PORT || 3000;
 
-// var server = app.listen(port, ip, function () {
-//         var host = server.address().address
-//         var port = server.address().port
-//         console.log('Example app listening at http://%s:%s', host, port)
-// });
-
-const http = require("http");
-var server = http.createServer(
-    (request,response) => {
-        response.end('hello js')
-    }
-    )
-    server.listen(port)
+var server = http.createServer((req,res) =>{
+    res.end('<html><body><h1>hello</h1></body></html>')
+});
+server.listen(8080, 'localhost');
