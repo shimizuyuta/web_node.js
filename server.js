7e0321b8d5a7a746b46162f78b1e0b68c8d2974c
EC2 Default User <ec2-user@ip-172-31-47-23.ap-northeast-1.compute.internal>
2020-11-10 07:52:44 +0000

    ejs

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const http = require("http")
 const port = process.env.PORT || 3000;
+const fs = require("fs");
+const req_res = (req,res) =>{
+    fs.readFile('./index2.html','UTF-8',
+    (error,data) =>{
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.write(data);
+        res.end();
+        console.log('hello world')
+    }
+    );
+};
 
-var server = http.createServer((req,res) =>{
-    res.end('<html><body><h1>hello</h1></body></html>')
-});
-server.listen(8080, 'localhost');
+
+
+var server = http.createServer(req_res)
+server.listen(port)
+
