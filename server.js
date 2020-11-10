3da5ec5db3f63ed24d5e2a7ec3aa9135bb792c8b
EC2 Default User <ec2-user@ip-172-31-47-23.ap-northeast-1.compute.internal>
2020-11-10 10:51:56 +0000

    css

--- server.js
+++ server.js
@@ -2,18 +2,42 @@ const http = require("http")
 const port = process.env.PORT || 3000;
 const fs = require("fs");
 const ejs = require("ejs")
+const url = require("url");
 
 const index_page = fs.readFileSync('./index.ejs','utf-8');
-
+const style_css = fs.readFileSync('./style.css','utf-8')
 var server = http.createServer(getFromClient)
+
+
 
 
 // const getFromClient = 
 function getFromClient(req,res) {
-    var content = ejs.render(index_page);
-    res.writeHead(200,{'Content-Type':'text/html'});
-    res.write(content)
-    res.end();
+    var url_parts =url.parse(req.url);
+    switch (url_parts.pathname) {
+        case '/':
+            var content = ejs.render(index_page,{
+                title:'hey yo',
+                content: 'bor'
+            });
+            res.writeHead(200,{'Content-Type': 'text/html'});
+            res.write(content);
+            res.end();
+            // code
+            break;
+            
+        case'/style.css':
+            res.writeHead(200,{'Content-Type': 'text/css'});
+            res.write(style_css);
+            res.end();
+            break;
+        
+        default:
+        res.writeHead(200,{'Contet-Type':'text/plan'});
+        res.end('no no no');
+        break;
+            // code
+    }
 }
     
 
