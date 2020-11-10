92f24c80579baadf2a7eb4a810759216aa777154
EC2 Default User <ec2-user@ip-172-31-47-23.ap-northeast-1.compute.internal>
2020-11-10 15:19:01 +0000

    基本操作

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const port = process.env.PORT || 3000;
 const fs = require("fs");
 const ejs = require("ejs")
 const url = require("url");
+const qs = require("querystring")
 
 const other_page = fs.readFileSync('other.ejs','utf-8')
 const index_page = fs.readFileSync('index.ejs','utf-8');
@@ -14,26 +15,32 @@ var server = http.createServer(getFromClient)
 
 // const getFromClient = 
 function getFromClient(req,res) {
-    var url_parts =url.parse(req.url);
+    var url_parts =url.parse(req.url,true);
     switch (url_parts.pathname) {
         case '/':
-            var content = ejs.render(index_page,{
-                title:'hey yo',
-                content: 'bor'
-            });
-            res.writeHead(200,{'Content-Type': 'text/html'});
-            res.write(content);
-            res.end();
-            // code
+            response_index(req,res);
             break;
+            
+            
+            // var content = ejs.render(index_page,{
+            //     title:'hey yo',
+            //     content: content
+            // });
+            // res.writeHead(200,{'Content-Type': 'text/html'});
+            // res.write(content);
+            // res.end();
+            // // code
+            // break;
         case '/other':
-            var content2 = ejs.render(other_page,{
-                title:'hy',
-                content:'bor'
-            });
-            res.writeHead(200,{'Content-Type': 'text/html'});
-            res.write(content2);
-            res.end();
+            // var content2 = ejs.render(other_page,{
+            //     title:'hy',
+            //     content:'bor'
+            // });
+            // res.writeHead(200,{'Content-Type': 'text/html'});
+            // res.write(content2);
+            // res.end();
+            // break;
+            response_other(req,res);
             break;
             
         case'/style.css':
@@ -50,7 +57,46 @@ function getFromClient(req,res) {
             // code
     }
 }
-    
+
+var data ={
+    'taro':'33',
+    'ziro':'333',
+    'zima':'zima'
+}
+
+function response_index(req,res){
+    var msg ='what the fuck' 
+    var content2 = ejs.render(index_page,{
+                title:'hy',
+                content: msg,
+                data: data,
+            });
+            res.writeHead(200,{'Content-Type': 'text/html'});
+            res.write(content2);
+            res.end();
+}
+var response_other = (req,res) =>{
+    var msg = 'これは厳格';
+    if (req.method === 'POST'){
+        var body ='';
+        
+        req.on('data',(data) =>{
+            body += data;
+        })
+        
+        req.on('end',() =>{
+            var post_data = qs.parse(body);
+            msg += 'you ' + post_data.msg;
+            var content = ejs.render(other_page,{
+                title:'ohter',
+                content:msg,
+            });
+             res.writeHead(200,{'Content-Type': 'text/html'});
+            res.write(content);
+            res.end();
+        })
+    }
+}
 
 
 
